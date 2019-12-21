4c0c6e2822e3bc4861c8d95aaabee87a7d7217c4
ZEXSM <zaxabor@gmail.com>
2019-12-21 18:01:28 +0300

    #ZEXSM#fix snapshot#add relations

--- src/DB.Core/Entities/Chat/ChatRoomEntity.cs
+++ src/DB.Core/Entities/Chat/ChatRoomEntity.cs
@@ -8,11 +8,14 @@ namespace DB.Core.Entities.Chat
     public class ChatRoomEntity : BaseEntity
     {
         [Required]
+        [MaxLength(250)]
         public string Name { get; set; }
 
         [Required]
         public DateTimeOffset CreateDate { get; set; }
 
         public List<UserEntity> Users { get; set; }
+
+        public List<ChatMessageEntity> ChatMessages { get; set; }
     }
 }
--- src/DB.Core/Entities/Identity/UserEntity.cs
+++ src/DB.Core/Entities/Identity/UserEntity.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DB.Core.Entities.Chat;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace DB.Core.Entities.Identity
@@ -6,18 +8,22 @@ namespace DB.Core.Entities.Identity
     public class UserEntity : BaseEntity
     {
         [Required]
+        [MaxLength(250)]
         public string Username { get; set; }
 
         [Required]
+        [MaxLength(250)]
         public string FirstName { get; set; }
 
         [Required]
+        [MaxLength(250)]
         public string LastName { get; set; }
 
         [Required]
         public string Password { get; set; }
 
         [Required]
+        [MaxLength(250)]
         public string Email { get; set; }
 
         [Required]
@@ -28,5 +34,7 @@ namespace DB.Core.Entities.Identity
 
         [Required]
         public DateTimeOffset UpdateDate { get; set; }
+
+        public List<ChatMessageEntity> ChatMessages { get; set; }
     }
 }
